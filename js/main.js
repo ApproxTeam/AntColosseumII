d89c157892c797b03b576a5b88a5928f43387063
Adam Raźniewski <adam.razniewski@gmail.com>
2017-09-14 15:19:05 +0200

    CSS update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function GameViewModel() {
+    var self = this;
+    self.resources = ko.observableArray([
+      {name: "Rocks", value: 0, icon: '<span class="fa fa-money"/>'},
+      {name: "Wood", value: 0, icon: '<span class="fa fa-money"/>'},
+      {name: "Bugs", value: 0, icon: '<span class="fa fa-bug"/>'}
+    ]);
+
+}
+
+
+
+var mainGameViewModel = new GameViewModel();
+ko.applyBindings(mainGameViewModel);
