510918fe12736269aca0e601d211ee044e5759b5
Adam Raźniewski <adam.razniewski@gmail.com>
2017-09-14 20:07:53 +0200

    added webSocket handler

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
 function GameViewModel() {
-    var self = this;
-    self.resources = ko.observableArray([
-      {name: "Rocks", value: 0, icon: '<span class="fa fa-money"/>'},
-      {name: "Wood", value: 0, icon: '<span class="fa fa-money"/>'},
-      {name: "Bugs", value: 0, icon: '<span class="fa fa-bug"/>'}
-    ]);
+  var self = this;
+  self.resources = ko.observableArray([{
+      name: "Rocks",
+      value: 0,
+      icon: '<span class="fa fa-money"/>'
+    },
+    {
+      name: "Wood",
+      value: 0,
+      icon: '<span class="fa fa-money"/>'
+    },
+    {
+      name: "Bugs",
+      value: 0,
+      icon: '<span class="fa fa-bug"/>'
+    }
+  ]);
+
+}
+
+function onClick() {
+  doSend(webSocket, "TEST");
+  console.log("LEO");
 
 }
 
 var mainGameViewModel = new GameViewModel();
 ko.applyBindings(mainGameViewModel);
+
+
+var socketHandler = {
+  onOpen: function(event) {
+    console.log(event);
+  },
+  onClose: function(event) {
+    console.log(event);
+  },
+  onMessage: function(event) {
+    console.log(event);
+  },
+  onError: function(event) {
+    console.log(event);
+  }
+}
+
+var webSocket = initSockets("ws://localhost:8080/OlympiaServer/game", socketHandler.onMessage, socketHandler.onOpen, socketHandler.onClose, socketHandler.onError);
---
+++ js/socketHandler.js
@@ -0,0 +1,14 @@
+function initSockets(url, onMessage, onOpen, onClose, onError)
+{
+    var webSocket = new WebSocket(url);
+    webSocket.onmessage = onMessage;
+    webSocket.onopen = onOpen;
+    webSocket.onclose = onClose;
+    webSocket.onerror = onError;
+    return webSocket;
+ }
+
+function doSend(socket, message)
+{
+	socket.send(message);
+}
