bd9d4f5063511ac16ea97ae90eea1642ae3e5c29
Adam Raźniewski <adam.razniewski@gmail.com>
2017-09-19 13:14:39 +0200

    Initialise game

--- src/js/gameRender/game.js
+++ src/js/gameRender/game.js
@@ -2,5 +2,16 @@ export function initialiseGame() {
   let mainCanvas = document.getElementById("mainGame");
   let app = new PIXI.Application(mainCanvas.width, mainCanvas.height, {view: mainCanvas});
   app.autoResize = true;
-  app.backgroundColor = 0x061639;
+  PIXI.loader.add('blob', './assets/blob.png').load(function(loader, resources) {
+    var blob = new PIXI.Sprite(resources.blob.texture);
+    blob.x = app.renderer.width / 2;
+    blob.y = app.renderer.height / 2;
+    blob.anchor.x = 0.5;
+    blob.anchor.y = 0.5;
+    app.stage.addChild(blob);
+
+    app.ticker.add(function() {
+        blob.rotation += 0.01;
+    });
+  });
 }
