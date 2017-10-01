b17e8d0b1d27f1a163163151c20420bef4b84667
Adam Raźniewski <adam.razniewski@gmail.com>
2017-10-01 10:48:43 +0200

    Default configuration

--- src/js/sockets/socketConfig.js
+++ src/js/sockets/socketConfig.js
@@ -2,7 +2,7 @@ import { initialiseGame, initialiseLoader } from '../gameRender/game.js';
 import { recognizeEvent } from '../gameRender/controller.js';
 
 export const socketConfiguration = {
-  url: "ws://localhost:8080/AntColosseumServer/game",
+  url: "ws://approxteam.ddns.net:7777/AntColosseumServer/game",
   onOpen: function(event) {
     initialiseLoader();
   },
