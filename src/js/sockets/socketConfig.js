8d90cf3abcf9d340c1222b51fd6d308590d5f7b1
Adam Raźniewski <adam.razniewski@gmail.com>
2017-10-03 09:22:05 +0200

    Socket config default change

--- src/js/sockets/socketConfig.js
+++ src/js/sockets/socketConfig.js
@@ -1,10 +1,12 @@
 import { initialiseGame, initialiseLoader } from '../gameRender/game.js';
 import { recognizeEvent } from '../gameRender/controller.js';
+import { conditionallySendActivate } from '../gameRender/controller.js';
 
 export const socketConfiguration = {
   url: "ws://approxteam.ddns.net:7777/AntColosseumServer/game",
   onOpen: function(event) {
     initialiseLoader();
+    conditionallySendActivate();
   },
   onClose: function(event) {
     console.log(event);
